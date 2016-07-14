1f9004a38eaf22dbaf1ac9664822e0b170c41799
Alexander Maringele <aXm@ydWLT-E-18-34.uibk.ac.at>
2016-07-14 08:18:17 +0200

    Update UtileAbsoluteTime to UtileTime

--- Sources/Utile.swift
+++ Sources/Utile.swift
@@ -5,23 +5,40 @@ import Glibc
 import Darwin
 #endif
 
+let ticksPerSecond = Double(sysconf(_SC_CLK_TCK))
 
 /// Substitute for CFAbsoluteTime which does not seem to be available on Linux.
-typealias UtileAbsoluteTime = Double
+typealias UtileTime = (user:Double,system:Double,absolute:Double)
 
 /// Substitute for CFAbsoluteTimeGetCurrent() which does not seem to be available on Linux.
-func UtileAbsoluteTimeGetCurrent() -> UtileAbsoluteTime {
-  var tval = timeval()                                              // C struct
-  let _ = gettimeofday(&tval,nil)                                   // will return 0
-  return Double(tval.tv_sec) + Double(tval.tv_usec)/1_000_000.0     // s + µs
+func UtileTimeGetCurrent() -> UtileTime {
+  var atime = timeval()                   // C struct
+  var ptime = tms()                       //
+  let _ = gettimeofday(&atime,nil)           // will return 0
+  let _ = times(&ptime)
+
+
+  return (
+    user:Double(ptime.tms_utime)/ticksPerSecond,
+    system:Double(ptime.tms_stime)/ticksPerSecond,
+    absolute:Double(atime.tv_sec) + Double(atime.tv_usec)/1_000_000.0     // s + µs
+  )
+}
+
+func -(lhs:UtileTime, rhs:UtileTime) -> UtileTime {
+  return (
+    user:lhs.user-rhs.user,
+  system:lhs.system-rhs.system,
+  absolute:lhs.absolute-rhs.absolute)
 }
 
 /// Measure the absolute runtime of a code block.
 /// Usage: `let (result,runtime) = measure { *code to measure* }`
-func measure<R>(f:()->R) -> (R, UtileAbsoluteTime) {
-  let start = UtileAbsoluteTimeGetCurrent()
+func measure<R>(f:()->R) -> (R, UtileTime) {
+  let start = UtileTimeGetCurrent()
   let result = f()
-  return (result, UtileAbsoluteTimeGetCurrent() - start)
+  let end = UtileTimeGetCurrent()
+  return (result, end - start)
 }
 
 struct UtileIterator<S,T> : IteratorProtocol {
