7f7f67bf710d32a95d3c2f5e965b29a516e93970
Alexander Maringele <aXm@ydWLT-E-18-34.uibk.ac.at>
2016-07-19 10:09:01 +0200

    Update Node + Properties

--- Sources/Node+Properties.swift
+++ Sources/Node+Properties.swift
@@ -9,21 +9,31 @@ extension Node {
     }
     return true
   }
+}
 
-  var subnodes: Set<Self> {
+extension Node {
+  var defaultSubnodes: Set<Self> {
     let singleton = Set(arrayLiteral:self)
     guard let nodes = self.nodes where nodes.count > 0 else {
       return singleton
     }
 
     return singleton.union(nodes.flatMap { $0.subnodes })
-
-
   }
 
-  var variables: Set<Self> {
+  var defaultVariables: Set<Self> {
     guard let nodes = self.nodes else { return Set(arrayLiteral:self) }
     return Set(nodes.flatMap { $0.variables })
   }
+}
 
+extension Node {
+
+  var variables: Set<Self> {
+    return defaultVariables
+  }
+
+  var subnodes: Set<Self> {
+    return defaultSubnodes
+  }
 }
