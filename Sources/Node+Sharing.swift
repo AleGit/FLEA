74a2b7df8f7c2823ffc6622774f98ccad546559c
Alexander Maringele <nyaya@maringele.at>
2016-07-27 17:55:45 +0200

    Add Kin and Sharing protocol

--- Sources/Node+Sharing.swift
+++ Sources/Node+Sharing.swift
@@ -6,6 +6,17 @@ protocol PartialSetAlgebra {
   func contains(_ member: Element) -> Bool
 }
 
+protocol Sharing : class {
+  associatedtype M : PartialSetAlgebra
+  static func share(node:Self) -> Self
+  static var allNodes : M { get set }
+}
+
+protocol Kin : class {
+    associatedtype P : WeakPartialSetAlgebra
+    var parents : P { get set }
+}
+
 protocol WeakPartialSetAlgebra : PartialSetAlgebra {}
 
 
@@ -17,9 +28,17 @@ extension WeakSet : WeakPartialSetAlgebra {}
 /// a collection of all created nodes is held.
 /// Unique instances of nodes are shared between (sub)trees.
 /// Sharing is suitable for immutable reference types.
-protocol SharingNode : class, Node {
-  associatedtype M : PartialSetAlgebra
-  static var allNodes : M { get set }
+protocol SharingNode : Sharing, Node {
+  // associatedtype M : PartialSetAlgebra
+  // static var allNodes : M { get set }
+}
+
+extension Node where Self:Sharing, Self.M.Element == Self {
+  /// By default smart nodes are shared between trees,
+  /// e.g. one unique instance of variable `X` in `p(X,f(X,X))`.
+  static func share(node:Self) -> Self {
+    return allNodes.insert(node).memberAfterInsert
+  }
 }
 
 /// If the collection is of the right type, perfect sharing will work automatically.
@@ -33,15 +52,14 @@ extension SharingNode where M.Element == Self {
 
 /// kin nodes are sharing, additionally
 /// they hold weak references to all their parents
-protocol KinNode : class, Node {
-  associatedtype M : PartialSetAlgebra
-  associatedtype P : WeakPartialSetAlgebra
-  static var allNodes : M { get set }
-  var parents : P { get set}
+protocol KinNode : Kin, Sharing, Node {
+  // associatedtype M : PartialSetAlgebra
+  // associatedtype P : WeakPartialSetAlgebra
+  // static var allNodes : M { get set }
+  // var parents : P { get set}
 }
 
-
-extension KinNode where M.Element == Self, P.Element == Self {
+extension Node where Self:Kin, Self:Sharing, Self.M.Element==Self, Self.P.Element==Self {
   static func share(node:Self) -> Self {
     let member = allNodes.insert(node).memberAfterInsert
 
@@ -53,3 +71,17 @@ extension KinNode where M.Element == Self, P.Element == Self {
     return member
   }
 }
+
+
+// extension KinNode where M.Element == Self, P.Element == Self {
+//   static func share(node:Self) -> Self {
+//     let member = allNodes.insert(node).memberAfterInsert
+//
+//     if let nodes = member.nodes {
+//       for n in nodes {
+//         let _ = n.parents.insert(member)
+//       }
+//     }
+//     return member
+//   }
+// }
