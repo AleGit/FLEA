b6f009da85189d0642040e27dd0fc9d633883adc
Alexander Maringele <aXm@ydWLT-E-18-34.uibk.ac.at>
2016-08-03 12:57:56 +0200

    Update LiteralConvertible

--- Sources/Node+Symbolable+LiteralConvertible.swift
+++ Sources/Node+Symbolable+LiteralConvertible.swift
@@ -21,18 +21,7 @@ extension Node where Symbol:Symbolable {
   /// we generate a single fol formula.
   init(stringLiteral value: StringLiteralType) {
     // let fof = "fof(name,role,predicate(\(value))."
-
-
-
-    // guard var file = tmpfile() else {
-    //   self.init(c:"failed")
-    // }
-    // defer { fclose(file) }
-
-
-
-
-
-    self.init(c:"failed")
+    self.init()
+    self = Self(c:"❌")
   }
 }
