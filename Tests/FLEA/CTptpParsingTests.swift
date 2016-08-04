6dc16611059cb0235b3e2f617d05256ff4ee3702
Alexander Maringele <aXm@ydWLT-E-18-34.uibk.ac.at>
2016-08-04 13:52:10 +0200

    Upade C api tests

--- Tests/FLEA/CTptpParsingTests.swift
+++ Tests/FLEA/CTptpParsingTests.swift
@@ -13,38 +13,50 @@ public class CTptpParsingApiTests : XCTestCase {
   func testStore() {
     prlcParsingStore = prlcCreateStore(1_000_000)
     guard let store = prlcParsingStore else {
-      print("Error: creation of store failed.")
+      print("Error: creation of store failed.",nok)
       return
     }
     defer {
       prlcDestroyStore(store)
     }
-    print("symbols",store.pointee.symbols.size)
-    print("p_nodes",store.pointee.p_nodes.size)
-    print("t_nodes",store.pointee.t_nodes.size)
+    XCTAssertEqual(42,store.pointee.symbols.size,nok)
+    XCTAssertEqual(20,store.pointee.p_nodes.size,nok)
+    XCTAssertEqual(0,store.pointee.t_nodes.size,nok)
+
+    XCTAssertEqual(333_333,store.pointee.symbols.capacity,nok)
+    XCTAssertEqual(1_000_000,store.pointee.p_nodes.capacity,nok)
+    XCTAssertEqual(333_333,store.pointee.t_nodes.capacity,nok)
   }
 
   func testTypes() {
-    print("=========================================================")
-    print("types:")
-    print("prlcParsingStore :", prlcParsingStore.dynamicType)
-    print("prlcParsingRoot :", prlcParsingRoot.dynamicType)
-    print(" ")
+    XCTAssertEqual(
+      "ImplicitlyUnwrappedOptional<UnsafeMutablePointer<prlc_store>>"
+      ,"\(prlcParsingStore.dynamicType)",nok)
+    XCTAssertEqual(
+      "ImplicitlyUnwrappedOptional<UnsafeMutablePointer<prlc_tree_node>>"
+      ,"\(prlcParsingRoot.dynamicType)",nok)
+    XCTAssertEqual(
+      "((ImplicitlyUnwrappedOptional<UnsafePointer<Int8>>, ImplicitlyUnwrappedOptional<UnsafeMutablePointer<Optional<UnsafeMutablePointer<prlc_store>>>>, ImplicitlyUnwrappedOptional<UnsafeMutablePointer<Optional<UnsafeMutablePointer<prlc_tree_node>>>>)) -> Int32"
+      ,"\(prlcParsePath.dynamicType)",nok)
+    XCTAssertEqual(
+      "(ImplicitlyUnwrappedOptional<UnsafeMutablePointer<prlc_store>>) -> ()"
+      ,"\(prlcDestroyStore.dynamicType)",nok)
+    XCTAssertEqual(
+      "((ImplicitlyUnwrappedOptional<UnsafeMutablePointer<prlc_store>>, ImplicitlyUnwrappedOptional<UnsafePointer<Int8>>)) -> ImplicitlyUnwrappedOptional<UnsafePointer<Int8>>"
+      ,"\(prlcStoreSymbol.dynamicType)",nok)
+    XCTAssertEqual(
+      "((ImplicitlyUnwrappedOptional<UnsafeMutablePointer<prlc_store>>, ImplicitlyUnwrappedOptional<UnsafePointer<Int8>>)) -> ImplicitlyUnwrappedOptional<UnsafePointer<Int8>>"
+      ,"\(prlcGetSymbol.dynamicType)",nok)
+    XCTAssertEqual(
+      "((ImplicitlyUnwrappedOptional<UnsafeMutablePointer<prlc_store>>, ImplicitlyUnwrappedOptional<UnsafePointer<Int8>>)) -> ImplicitlyUnwrappedOptional<UnsafePointer<Int8>>"
+      ,"\(prlcGetSymbol.dynamicType)",nok)
+    XCTAssertEqual(
+      "((ImplicitlyUnwrappedOptional<UnsafeMutablePointer<prlc_store>>, ImplicitlyUnwrappedOptional<UnsafePointer<Int8>>)) -> ImplicitlyUnwrappedOptional<UnsafePointer<Int8>>"
+      ,"\(prlcGetSymbol.dynamicType)",nok)
 
-    print("prlcParsePath :", prlcParsePath.dynamicType)
-    print("prlcDestroyStore :", prlcDestroyStore.dynamicType)
-    print(" ")
-    print("prlcStoreSymbol :", prlcStoreSymbol.dynamicType)
-    print("prlcGetSymbol :", prlcGetSymbol.dynamicType)
-    print("prlcFirstSymbol :", prlcGetSymbol.dynamicType)
-    print("prlcNextSymbol :", prlcGetSymbol.dynamicType)
-
-
-    print(" ")
-
-    print("prlcStoreNodeFile :", prlcStoreNodeFile.dynamicType)
-
-    print("=========================================================")
+    XCTAssertEqual(
+      "((ImplicitlyUnwrappedOptional<UnsafeMutablePointer<prlc_store>>, ImplicitlyUnwrappedOptional<UnsafePointer<Int8>>)) -> ImplicitlyUnwrappedOptional<UnsafeMutablePointer<prlc_tree_node>>"
+      ,"\(prlcStoreNodeFile.dynamicType)",nok)
 
 
   }
