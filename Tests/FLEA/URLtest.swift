e7a5fcbaee6d910efbaa5d03049db7a116a9514a
Alexander <ubox@maringele.at>
2016-08-09 09:41:03 +0200

    Update URL tests for unix

--- Tests/FLEA/URLtest.swift
+++ Tests/FLEA/URLtest.swift
@@ -7,7 +7,7 @@ import Foundation
 public class URLTests : XCTestCase {
   static var allTests : [(String, (URLTests) -> () throws -> Void)] {
     return [
-      ("testFiles",testFiles)
+    ("testFiles",testFiles)
     ]
   }
 
@@ -25,49 +25,73 @@ public class URLTests : XCTestCase {
     for url in [
     URL(fileURLWithPath:"Problems/PUZ001-1.p"),
     URL(fileURLWithPath:"~/TPTP/Problems/PUZ001+1.p")
-     ] {
-       print("")
-       #if os(OSX)
-    print("url                            ", url)
-    print("var absoluteString: String     ", url.absoluteString)
-    print("var absoluteURL: URL           ", url.absoluteURL)
-    print("var baseURL: URL?              ", url.baseURL ?? "n/a")
-    print("var fragment: String?          ", url.fragment ?? "n/a")
-    // print("var hasDirectoryPath: Bool     ",url.hasDirectoryPath) // OSX >=10.11
-    print("var hashValue: Int             ", url.hashValue)
-    print("var host: String?              ", url.host ?? "n/a")
-    print("var isFileURL: Bool            ", url.isFileURL)
-    print("var lastPathComponent: String  ", url.lastPathComponent)
-    print("var password: String?          ", url.password ?? "n/a")
-    print("var path: String               ", url.path)
-    print("var pathComponents: [String]   ", url.pathComponents)
-    print("var pathExtension: String      ", url.pathExtension)
-    print("var port: Int?                 ", url.port)
-    print("var query: String?             ", url.query ?? "n/a")
-    print("var relativePath: String       ", url.relativePath)
-    print("var relativeString: String     ", url.relativeString)
-    print("var scheme: String?            ", url.scheme ?? "n/a")
-    print("var user: String?              ", url.user ?? "n/a")
-    print("var standardized: URL          ", url.standardized)
-    print("var standardizedFileURL: URL   ", url.standardizedFileURL)
-    //
-    //
-    print("func checkPromisedItemIsReachable()  ",(try? url.checkPromisedItemIsReachable()))
-    print("func checkResourceIsReachable()      ",(try? url.checkResourceIsReachable()))
-    //
-    print("func deletingLastPathComponent()     ",url.deletingLastPathComponent().path)
-    print("func deletingPathExtension()         ",url.deletingPathExtension().path)
-    print("func resolvingSymlinksInPath()       ",url.resolvingSymlinksInPath().path)
+    ] {
+      print("")
+      #if os(OSX)
+      print("url                            ", url)
+      print("var absoluteString: String     ", url.absoluteString)
+      print("var absoluteURL: URL           ", url.absoluteURL)
+      print("var baseURL: URL?              ", url.baseURL)
+      print("var fragment: String?          ", url.fragment ?? "nil")
+      // print("var hasDirectoryPath: Bool     ",url.hasDirectoryPath) // OSX >=10.11
+      print("var hashValue: Int             ", url.hashValue)
+      print("var host: String?              ", url.host ?? "nil")
+      print("var isFileURL: Bool            ", url.isFileURL)
+      print("var lastPathComponent: String  ", url.lastPathComponent)
+      print("var password: String?          ", url.password ?? "nil")
+      print("var path: String               ", url.path)
+      print("var pathComponents: [String]   ", url.pathComponents)
+      print("var pathExtension: String      ", url.pathExtension)
+      print("var port: Int?                 ", url.port)
+      print("var query: String?             ", url.query ?? "nil")
+      print("var relativePath: String       ", url.relativePath)
+      print("var relativeString: String     ", url.relativeString)
+      print("var scheme: String?            ", url.scheme ?? "nil")
+      print("var user: String?              ", url.user ?? "nil")
+      print("var standardized: URL          ", url.standardized)
+      print("var standardizedFileURL: URL   ", url.standardizedFileURL)
+      //
+      print("func checkPromisedItemIsReachable()  ",(try? url.checkPromisedItemIsReachable()))
+      print("func checkResourceIsReachable()      ",(try? url.checkResourceIsReachable()))
+      //
+      print("func deletingLastPathComponent()     ",url.deletingLastPathComponent().path)
+      print("func deletingPathExtension()         ",url.deletingPathExtension().path)
+      print("func resolvingSymlinksInPath()       ",url.resolvingSymlinksInPath().path)
 
+      #elseif os(Linux)
+      print("url                            ", url)
+      print("var absoluteString: String?    ", url.absoluteString ?? "nil")
+      print("var absoluteURL: URL?          ", url.absoluteURL)
+      print("var baseURL: URL?              ", url.baseURL)
+      print("var fragment: String?          ", url.fragment ?? "nil")
+      // print("var hasDirectoryPath: Bool     ",url.hasDirectoryPath) // OSX >=10.11
+      print("var hashValue: Int             ", url.hashValue)
+      print("var host: String?              ", url.host ?? "nil")
+      print("var isFileURL: Bool            ", url.isFileURL)
+      print("var lastPathComponent: String  ", url.lastPathComponent)
+      print("var password: String?          ", url.password ?? "nil")
+      print("var path: String               ", url.path)
+      print("var pathComponents: [String]   ", url.pathComponents)
+      print("var pathExtension: String      ", url.pathExtension)
+      print("var port: Int?                 ", url.port)
+      print("var query: String?             ", url.query ?? "nil")
+      print("var relativePath: String       ", url.relativePath)
+      print("var relativeString: String     ", url.relativeString)
+      print("var scheme: String?            ", url.scheme ?? "nil")
+      print("var user: String?              ", url.user ?? "nil")
+      print("var standardized: URL          ", url.standardized)
+      print("var standardizedFileURL: URL   ", "n/a") // url.standardizedFileURL) // n\a
+      //
+      print("func checkPromisedItemIsReachable()  ", "n/a") // (try? url.checkPromisedItemIsReachable())) // n\a
+      print("func checkResourceIsReachable()      ", "n/a") // (try? url.checkResourceIsReachable())) // n\a
+      //
+      print("func deletingLastPathComponent()     ",(try? url.deletingLastPathComponent())?.path ?? "nil")
+      print("func deletingPathExtension()         ",(try? url.deletingPathExtension())?.path ?? "nil")
+      print("func resolvingSymlinksInPath()       ",(try? url.resolvingSymlinksInPath())?.path ?? "nil")
 
+      #endif
 
-    // print("func standardizingPath()             ",try? url.standardizingPath())
-    // print(" ",url.host)
-    // print(" ",url.host)
-    #elseif os(Linux)
-    #endif
-
-}
+    }
 
   }
 }
