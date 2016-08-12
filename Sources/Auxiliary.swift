f3ca1ee535108e78aad0150a8c47402ac3217698
Alexander Maringele <nyaya@maringele.at>
2016-08-12 07:53:02 +0200

    Update Auxiliary

--- Sources/Auxiliary.swift
+++ Sources/Auxiliary.swift
@@ -8,7 +8,7 @@ extension Collection where Iterator.Element == SubSequence.Iterator.Element {
   /// - [] -> nil
   /// - [a,...] -> (a,[...])
   ///
-  /// _Complexity_: O(1) -- both first and dropFirst() are O(1)
+  /// _Complexity_: O(1) -- `first` and `dropFirst()` are O(1) for collection
   var decomposing: (head: Self.Iterator.Element, tail: Self.SubSequence)? {
     guard let head = first else { return nil }
     return (head, dropFirst()) //
@@ -16,22 +16,24 @@ extension Collection where Iterator.Element == SubSequence.Iterator.Element {
 }
 
 extension Sequence {
+  /// check if a predicate holds for all members of a sequence
+  func all(_ predicate: (Iterator.Element) -> Bool) -> Bool {
+    return self.reduce(true) { $0 && predicate($1) }
+  }
 
-    func all(_ predicate: (Iterator.Element) -> Bool) -> Bool {
-        return self.reduce(true) { $0 && predicate($1) }
-    }
+  /// check if a predicate holds for at least one member of a sequence
+  func one(_ predicate: (Iterator.Element) -> Bool) -> Bool {
+    return self.reduce(false) { $0 || predicate($1) }
+  }
 
-    func one(_ predicate: (Iterator.Element) -> Bool) -> Bool {
-        return self.reduce(false) { $0 || predicate($1) }
-    }
-
-    func count(_ predicate: (Iterator.Element) -> Bool = { _ in true }) -> Int {
-        return self.reduce(0) { $0 + (predicate($1) ? 1 : 0) }
-    }
+  /// count the members of a sequence where a predicate holds
+  func count(_ predicate: (Iterator.Element) -> Bool = { _ in true }) -> Int {
+    return self.reduce(0) { $0 + (predicate($1) ? 1 : 0) }
+  }
 }
 
 extension String {
-  /// Check if the string has an uppercase character at given index.
+  /// check if the string has an uppercase character at given index.
   func isUppercased(at: Index) -> Bool {
     let range = at..<self.index(after: at)
     return self.rangeOfCharacter(from: .uppercaseLetters, options: [], range: range) != nil
@@ -39,10 +41,12 @@ extension String {
 }
 
 extension String  {
-    func containsOne<S:Sequence where S.Iterator.Element == String>(_ strings:S) -> Bool {
-        return strings.reduce(false) { $0 || self.contains($1) }
-    }
-    func containsAll<S:Sequence where S.Iterator.Element == String>(_ strings:S) -> Bool {
-        return strings.reduce(true) { $0 && self.contains($1) }
-    }
+  /// check if at least on member of a sequence is a substring of the string
+  func containsOne<S:Sequence where S.Iterator.Element == String>(_ strings:S) -> Bool {
+    return strings.reduce(false) { $0 || self.contains($1) }
+  }
+  /// check if all members of a sequence are substrings of the string
+  func containsAll<S:Sequence where S.Iterator.Element == String>(_ strings:S) -> Bool {
+    return strings.reduce(true) { $0 && self.contains($1) }
+  }
 }
--- Tests/FLEA/AuxiliaryTests.swift
+++ Tests/FLEA/AuxiliaryTests.swift
@@ -6,32 +6,46 @@ public class AuxiliaryTests : XCTestCase {
   static var allTests : [(String, (AuxiliaryTests) -> () throws -> Void)] {
     return [
       ("testDecomposing", testDecomposing),
+      ("testAllOneCount", testAllOneCount),
       ("testUppercased", testUppercased),
+      ("testStringContains", testStringContains),
       ("testIds",testIds)
     ]
   }
 
   func testDecomposing() {
     let array = [11,12,13]
-    print(array)
     guard let (h1,t1) = array.decomposing else {
       XCTFail(nok)
       return
     }
-    print(h1,t1)
+    XCTAssertEqual(11,h1,nok)
     guard let (h2,t2) = t1.decomposing else {
         XCTFail(nok)
         return
     }
-    print(h2,t2)
+    XCTAssertEqual(12,h2,nok)
 
     guard let (h3,t3) = t2.decomposing else {
         XCTFail(nok)
         return
     }
-    print(h3,t3)
+    XCTAssertEqual(13,h3,nok)
 
-    print(t3.decomposing)
+    XCTAssertNil(t3.decomposing,nok)
+  }
+
+  func testAllOneCount() {
+    let array = [2,3,5,7,11,13]
+    XCTAssertFalse(array.all { $0 % 2 == 0},nok)
+    XCTAssertTrue(array.one { $0 % 2 == 0},nok)
+
+    XCTAssertTrue(array.all { $0 > 1},nok)
+    XCTAssertFalse(array.one { $0 < 1},nok)
+
+    XCTAssertEqual(1, array.count { $0 % 2 == 0},nok)
+    XCTAssertEqual(array.count,array.count { $0 > 1},nok)
+    XCTAssertEqual(0, array.count { $0 < 1 },nok)
   }
 
   func testUppercased() {
@@ -40,6 +54,21 @@ public class AuxiliaryTests : XCTestCase {
 
     XCTAssertTrue(u.isUppercased(at:u.startIndex),nok)
     XCTAssertFalse(l.isUppercased(at:l.startIndex),nok)
+  }
+
+  func testStringContains() {
+    let string = "Héllo, Wörld!"
+
+    XCTAssertTrue(string.containsAll ([String]()), nok)
+    XCTAssertFalse(string.containsOne ([String]()), nok)
+
+    XCTAssertTrue(string.containsAll(string.characters.map{String($0)}), nok)
+    XCTAssertTrue(string.containsOne(string.characters.map{String($0)}), nok)
+
+    XCTAssertTrue(string.containsOne (["x", "ä", "é"]),nok)
+    XCTAssertFalse(string.containsAll (["x", "ä", "é"]),nok)
+    XCTAssertFalse(string.containsOne (["x", "ä", "?"]),nok)
+    XCTAssertTrue(string.containsAll (["o", "ö", "!"]),nok)
   }
 
   func testIds() {
