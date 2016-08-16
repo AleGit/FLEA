063d79dc31f624544ebbde57fe611fd20d821f7d
Alexander Maringele <github@maringele.at>
2016-08-16 13:38:07 +0200

    Update smart node tests

--- Tests/FLEA/SmartNodeTests.swift
+++ Tests/FLEA/SmartNodeTests.swift
@@ -5,7 +5,7 @@ import XCTest
 /// local minimal implementation of protocol
 /// to avoid side effects (pool) from ohter test classes
 private final class SmartNode : FLEA.SharingNode {
-  typealias S = String // choose the symbol
+  typealias S = FLEA.Tptp.Symbol // choose the symbol
 
   static var pool = WeakSet<SmartNode>()
 
@@ -36,12 +36,12 @@ public class SmartNodeTests : XCTestCase {
   /// accumulate four distict nodes
   func testEqualityX() {
 
-    let X = Node(variable:"X")
-    let a = Node(constant:"a")
-    let fX = Node(symbol:"f", nodes:[X])
-    let fa = Node(symbol:"f", nodes:[a])
+    let X = Node(v:"X")
+    let a = Node(c:"a")
+    let fX = Node(f:"f", [X])
+    let fa = Node(f:"f", [a])
 
-    let fX_a = fX * [Node(variable:"X"):Node(constant:"a")]
+    let fX_a = fX * [Node(v:"X"):Node(c:"a")]
 
     XCTAssertEqual(fX_a,fa)
     XCTAssertTrue(fX_a == fa)
@@ -56,12 +56,12 @@ public class SmartNodeTests : XCTestCase {
   /// accumulate four distict nodes
   func testEqualityY() {
 
-    let X = Node(variable:"Y")
-    let a = Node(constant:"a")
-    let fX = Node(symbol:"f", nodes:[X])
-    let fa = Node(symbol:"f", nodes:[a])
+    let X = Node(v:"Y")
+    let a = Node(c:"a")
+    let fX = Node(f:"f", [X])
+    let fa = Node(f:"f", [a])
 
-    let fX_a = fX * [Node(variable:"Y"):Node(constant:"a")]
+    let fX_a = fX * [Node(v:"Y"):Node(c:"a")]
 
     XCTAssertEqual(fX_a,fa)
     XCTAssertTrue(fX_a == fa)
