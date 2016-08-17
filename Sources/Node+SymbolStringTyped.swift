afd04e617e09ec2c5071c2845dd7022f3552b544
Alexander Maringele <github@maringele.at>
2016-08-17 07:20:52 +0200

    Update SymbolStringType (cleanup)

--- Sources/Node+SymbolStringTyped.swift
+++ Sources/Node+SymbolStringTyped.swift
@@ -1,3 +1,14 @@
+/// default implementations where Node : SymbolStringTyped
+extension Node where Symbol:StringSymbolable {
+  var symbolStringType : (String,Tptp.SymbolType) {
+    return (self.symbol.string, self.symbol.type)
+  }
+
+  static func symbolize(string:String, type:Tptp.SymbolType) -> Symbol {
+    return Symbol(string,type)
+  }
+}
+
 /// provide default implementations for Node : SymbolStringTyped
 extension Node where Self:StringSymbolTabulating, Symbol == Self.Symbols.Symbol {
   var symbolStringType : (String,Tptp.SymbolType) {
@@ -8,3 +19,5 @@ extension Node where Self:StringSymbolTabulating, Symbol == Self.Symbols.Symbol 
     return self.symbols.insert(string, type)
   }
 }
+
+
