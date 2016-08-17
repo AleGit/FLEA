c983f17e89c994b86eff30c2032a297df1023f85
Alexander Maringele <github@maringele.at>
2016-08-17 11:45:21 +0200

    Update substitution (cleanup)

--- Sources/Node+Substitution.swift
+++ Sources/Node+Substitution.swift
@@ -1,6 +1,6 @@
 /*** This file could move to an own nodes module because Node.Symbol:Hashable only. ***/
 
-/// A substitution is a assignment from variables to terms.
+/// A substitution is a mapping from keys to values, e.g. a dictionary.
 protocol Substitution : ExpressibleByDictionaryLiteral, Sequence, CustomStringConvertible
 {
   associatedtype K : Hashable
@@ -18,23 +18,37 @@ extension Dictionary : Substitution {
   }
 }
 
+/// A node substitution has a specialized description.
+extension Substitution // K == V not necessary
+where K:Node, V:Node, Iterator == DictionaryIterator<K,V> {
+  var description : String {
+    let pairs = self.map { "\($0)->\($1)"  }.joined(separator:",")
+    return "{\(pairs)}"
+  }
+}
+
 /// 't * σ' returns the application of substitution σ on term t.
+/// *caution*: this implementation is more general as 
+/// the usual definition of substitution, where only variables
+/// are substituted with terms. Here any arbitrary subterm can be 
+/// replaced with an other term.
 func *<N:Node, S:Substitution>(t:N, σ:S) -> N 
 where N == S.K, N == S.V, S.Iterator == DictionaryIterator<N,N> { 
 
-    // assert(σ.isSubstitution)
-    if let tσ = σ[t] { return tσ }      // t is (variable) in σ.keys
+    if let tσ = σ[t] { return tσ } // t is (variable or term) in σ.keys
 
-    guard let nodes = t.nodes
-    else { return t } // t is a variable not in σ or has no children
+    guard let nodes = t.nodes else {
+      // t is a variable, but in in σ
+      return N(variable:t.symbol) // no implicit sharing!
+    }
 
     return N(symbol:t.symbol, nodes: nodes.map { $0 * σ })
 }
 
 
-/// concationation of substitutions (specialized for Substitution)
+/// concationation of substitutions
 func *<N:Node, S:Substitution>(lhs:S,rhs:S) -> S? 
-where S.K==N,S.V==N, S.Iterator==DictionaryIterator<N,N> {
+where N == S.K, N==S.V, S.Iterator==DictionaryIterator<N,N> {
 
   var subs = S()
   for (key,value) in lhs {
@@ -57,8 +71,59 @@ where S.K==N,S.V==N, S.Iterator==DictionaryIterator<N,N> {
   return subs
 }
 
+/// 't * s' returns the substitution of all variables in t with term s.
+func *<N:Node>(t:N, s:N) -> N {
+    guard let nodes = t.nodes else { return s } // a variable
+
+    return N(symbol:t.symbol, nodes: nodes.map { $0 * s })
+}
+
 /// 't⊥' returns the substitution of all variables in t with constant '⊥'.
 postfix func ⊥<N:Node>(t:N) -> N 
 where N:SymbolStringTyped {
     return t * N(c:"⊥")
 }
+
+/*
+extension Dictionary where Key:Node, Value:Node { // , Key == Value does not work
+    /// Do the runtime types of keys and values match?
+    private var isHomogenous : Bool {
+        return self.keys.first?.dynamicType == self.values.first?.dynamicType
+    }
+
+    /// Are *variables* mapped to terms?
+    private var allKeysAreVariables : Bool {
+        return Array(self.keys).reduce(true) {
+            $0 && $1.nodes == nil
+        }
+    }
+
+    /// Are terms mapped to *variables*?
+    private var allValuesAreVariables : Bool {
+        return Array(self.values).reduce(true) {
+            $0 && $1.nodes == nil
+        }
+    }
+
+    /// Are distinct terms mapped to *distinguishable* terms?
+    private var isInjective : Bool {
+        return self.keys.count == Set(self.values).count
+    }
+
+    /// A substitution maps variables to terms.
+    var isSubstitution : Bool {
+        return allKeysAreVariables
+    }
+
+    /// A variable substitution maps variables to variables.
+    var isVariableSubstitution : Bool {
+        return allKeysAreVariables && allValuesAreVariables
+    }
+
+    /// A (variable) renaming maps distinct variables to distinguishable variables.
+    var isRenaming : Bool {
+        return allKeysAreVariables && allValuesAreVariables && isInjective
+    }
+}
+
+*/
