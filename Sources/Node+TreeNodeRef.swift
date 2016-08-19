ef07b83cdc7b0d63e8c43215b55f3de7dcec0653
Alexander Maringele <nyaya@maringele.at>
2016-08-19 20:48:43 +0200

    Update Node+TreeNodeRef

--- Sources/Node+TreeNodeRef.swift
+++ Sources/Node+TreeNodeRef.swift
@@ -4,12 +4,15 @@ import CTptpParsing
 extension Node where Self:SymbolStringTyped {
   /// symbol string types can be easily initilized with a tree node reference
   init(tree:TreeNodeRef) {
+    let type = Tptp.SymbolType(of:tree)
 
-    let symbol = Self.symbolize(string:tree.symbol ?? "", type:Tptp.SymbolType(of:tree))
+    let symbol = Self.symbolize(string:tree.symbol ?? "n/a", type:type)
 
     switch tree.type {
     case PRLC_VARIABLE:
       assert (tree.child == nil)
+      assert (type == .variable)
+      
       self.init(variable:symbol)
     default:
       let nodes = tree.children.map { Self(tree:$0) }
