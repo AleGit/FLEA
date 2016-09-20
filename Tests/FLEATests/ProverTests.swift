327616a46e3033e252a8de677c1a4d8f24623525
Alexander Maringele <aXm@ydWLT-E-19-67.uibk.ac.at>
2016-09-20 09:33:08 +0200

    Update Prover (style)

--- Tests/FLEATests/ProverTests.swift
+++ Tests/FLEATests/ProverTests.swift
@@ -4,10 +4,10 @@ import XCTest
 
 /// Test the accumulation of nodes in SmartNode.pool.
 /// Nodes MUST NOT accumulate between tests.
-public class ProverTests : YicesTestCase {
+public class ProverTests: YicesTestCase {
 
   /// Collect all tests by hand for Linux.
-  static var allTests : [(String, (ProverTests) -> () throws -> Void)]  {
+  static var allTests: [(String, (ProverTests) -> () throws -> Void)]  {
     return [
       ("testInitPUZ001c1", testInitPUZ001c1),
       ("testInitPUZ062c1", testInitPUZ062c1),
@@ -16,16 +16,17 @@ public class ProverTests : YicesTestCase {
   }
 
   // local private adoption of protocol to avoid any side affects
-  private final class N : SymbolStringTyped, SymbolTabulating, Sharing, Kin, Node, ExpressibleByStringLiteral {
+  private final class N: SymbolStringTyped, SymbolTabulating, Sharing, Kin, Node,
+  ExpressibleByStringLiteral {
     typealias S = Int
     static var symbols = StringIntegerTable<S>()
     static var pool = WeakSet<N>()
     var folks = WeakSet<N>()
 
-    var symbol : S = N.symbolize(string:Tptp.wildcard, type:.variable)
-    var nodes : [N]? = nil
+    var symbol: S = N.symbolize(string:Tptp.wildcard, type:.variable)
+    var nodes: [N]? = nil
 
-    var description : String { return defaultDescription }
+    var description: String { return defaultDescription }
   }
 
 
@@ -38,9 +39,9 @@ private typealias Prover = ΠρῶτοςProver<N>
           return
       }
 
-      let (name,_) = prover.problem
+      let (name, _) = prover.problem
 
-      XCTAssertEqual(problem,name)
+      XCTAssertEqual(problem, name)
       XCTAssertEqual(12, prover.clauses.count)
       XCTAssertEqual(0, prover.includes.count)
 
@@ -49,25 +50,25 @@ private typealias Prover = ΠρῶτοςProver<N>
       prover.collect()
 
       XCTAssertFalse(prover.names.isEmpty)
-      XCTAssertEqual(12,prover.names.allValues.count)
+      XCTAssertEqual(12, prover.names.allValues.count)
 
       let nc = "prove_neither_charles_nor_butler_did_it"
       print(prover.names.retrieve(from:nc.characters))
 
 
-      XCTAssertEqual(2,prover.roles.count)
-      XCTAssertEqual(11,prover.roles[.hypothesis]?.count)
-      XCTAssertEqual(1,prover.roles[.negated_conjecture]?.count)
+      XCTAssertEqual(2, prover.roles.count)
+      XCTAssertEqual(11, prover.roles[.hypothesis]?.count)
+      XCTAssertEqual(1, prover.roles[.negated_conjecture]?.count)
 
-      XCTAssertEqual(4,prover.sizes.count) // i.e arities up to 3
+      XCTAssertEqual(4, prover.sizes.count) // i.e arities up to 3
       XCTAssertEqual(0, prover.sizes[0].count) // no empty clauses
       XCTAssertEqual(5, prover.sizes[1].count) // 5 unit clauses
       XCTAssertEqual(5, prover.sizes[2].count) // 5 clauses with two literals
       XCTAssertEqual(2, prover.sizes[3].count) // 2 clauses with three literals
 
-      XCTAssertEqual(Set([0,1,2,6,7]), prover.sizes[1])
-      XCTAssertEqual(Set([3,4,8,9,11]), prover.sizes[2])
-      XCTAssertEqual(Set([5,10]), prover.sizes[3])
+      XCTAssertEqual(Set([0, 1, 2, 6, 7]), prover.sizes[1])
+      XCTAssertEqual(Set([3, 4, 8, 9, 11]), prover.sizes[2])
+      XCTAssertEqual(Set([5, 10]), prover.sizes[3])
   }
 
   func testInitPUZ062c1() {
@@ -77,9 +78,9 @@ private typealias Prover = ΠρῶτοςProver<N>
           return
       }
 
-      let (name,_) = prover.problem
+      let (name, _) = prover.problem
 
-      XCTAssertEqual(problem,name)
+      XCTAssertEqual(problem, name)
       XCTAssertEqual(17, prover.clauses.count)
       XCTAssertEqual(2, prover.includes.count)
 
