2e9e662bdf25c9323ac6558783fef492df3aee97
Alexander Maringele <aXm@ydWLT-E-19-67.uibk.ac.at>
2016-09-21 13:07:36 +0200

    Update provers

--- Sources/Prover.swift
+++ Sources/Prover.swift
@@ -1,16 +1,30 @@
 import Foundation
+import CYices
+
+// MARK: protocols
 
 protocol Prover {
     /// Initialize Prover with problem name, e.g. 'PUZ001-1':
     /// Returns nil if problem file cannot be found or parsed.
     init?(problem name: String)
 
-    /// - true if set of clauses are saturated and satisfiable.
-    /// - false if set of clauses is unsatisfiable.
-    /// - nil in timeout was reached.
+    /// returns nil if timeout was reached before an a proof was found
+    /// returns true if problem is a theorem, e.g. negated conjecture is unsatisfiable (with proof)
+    /// returns false if problem is not a theorem, e.g. set of clause is saturated and satisfiable
     func run(timeout: AbsoluteTime) -> Bool?
-
 }
+
+protocol YicesProver: Prover {
+    func yicesLiterals(clauseIndex: Int) -> Set<term_t>
+
+    /// returns nil, if none of the literals holds
+    func selectedYicesLiteral(clauseIndex: Int) -> term_t?
+
+    /// returns nil if yicesLiteral not in yicesLiterals(clauseIndex: clauseIndex)
+    func selectedLiteralIndex(clauseIndex: Int, yicesLiteral: term_t) -> Int?
+}
+
+// MARK: - protocol extensions
 
 extension Prover {
     static func URLAndFile(problem name: String) -> (URL, Tptp.File)? {
---
+++ Sources/ProverY.swift
@@ -0,0 +1,6 @@
+import Foundation
+import CYices
+
+public final class ProverY {
+
+}
