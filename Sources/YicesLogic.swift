7f06519e2e643002cb80fcb91fa2f607c1625f0b
sarah <sarah.winkler@uibk.ac.at>
2016-10-07 15:36:21 +0200

    split Logic protocol into LogicExpr and LogicContext

--- Sources/YicesLogic.swift
+++ Sources/YicesLogic.swift
@@ -1,7 +1,72 @@
 import CYices
 
-class YicesLogic : Logic {
-  typealias Term = term_t
+final class YicesExpr : LogicExpr {
+  fileprivate var expr: term_t
+
+  init(_ e: term_t) {
+    self.expr = e
+  }
+
+  func not() -> YicesExpr {
+    return YicesExpr(yices_not(expr))
+  }
+
+  func and(_ t: YicesExpr) -> YicesExpr {
+    return YicesExpr(yices_and2(self.expr, t.expr))
+  }
+
+  func or(_ t: YicesExpr) -> YicesExpr {
+    return YicesExpr(yices_or2(self.expr, t.expr))
+  }
+
+  func implies(_ t: YicesExpr) -> YicesExpr {
+    return YicesExpr(yices_implies(self.expr, t.expr))
+  }
+
+  func ite(_ t: YicesExpr, _ f: YicesExpr) -> YicesExpr {
+      return YicesExpr(yices_ite(self.expr, t.expr, f.expr))
+  }
+
+  // arithmetic operators
+  func eq(_ t: YicesExpr) -> YicesExpr {
+    return YicesExpr(yices_eq(self.expr, t.expr))
+  }
+
+  func neq(_ t: YicesExpr) -> YicesExpr {
+    return YicesExpr(yices_neq(self.expr, t.expr))
+  }
+
+  func gt(_ t: YicesExpr) -> YicesExpr {
+    return YicesExpr(yices_arith_gt_atom(self.expr, t.expr))
+  }
+
+  func ge(_ t: YicesExpr) -> YicesExpr {
+    return YicesExpr(yices_arith_geq_atom(self.expr, t.expr))
+  }
+
+  func add(_ t: YicesExpr) -> YicesExpr {
+    return YicesExpr(yices_add(self.expr, t.expr))
+  }
+  
+  
+	// term deconstruction
+  /// Get children of a yices term.
+  var children : [YicesExpr] {
+    return (0..<yices_term_num_children(self.expr)).map {
+      YicesExpr(yices_term_child(self.expr, $0))
+    }
+  }
+
+  /*var subterms : Set<YicesExpr> {
+    var terms = Set(children().flatMap { subterms($0) })
+    terms.insert(self)
+    return terms
+  }*/
+}
+
+
+class YicesLogic : LogicContext {
+  typealias Expr = YicesExpr
   typealias TType = type_t
   typealias Model = OpaquePointer
 
@@ -11,90 +76,49 @@ class YicesLogic : Logic {
   let bool_type = yices_bool_type()
   let int_type = yices_int_type()
   let free_type = YicesLogic.namedType("𝛕")
-	// constants
-	var 🚧 : Term = YicesLogic.typedSymbol("⊥", YicesLogic.namedType("𝛕"))
+
+	// special constants
+	var 🚧 : Expr = YicesLogic.typedSymbol("⊥", YicesLogic.namedType("𝛕"))
 
   init() {
     ctx = yices_new_context(nil)
   }
 
 	deinit {
-		yices_free_context(ctx)
+	  yices_free_context(ctx)
 	}
 
 	static var versionString: String {
 		return String(validatingUTF8:yices_version) ?? "n/a"
 	}
 
-  // logical operators
-  var top = yices_true()
-  var bot = yices_false()
+  // create terms
+  var mkTop = YicesExpr(yices_true())
+  var mkBot = YicesExpr(yices_false())
 
-  func not(_ t: Term) -> Term {
-    return yices_not(t)
+  func mkOr(_ ts: [Expr]) -> Expr {
+    var ts_copy = ts.map{ $0.expr }
+    return YicesExpr(yices_or(UInt32(ts.count), &ts_copy))
   }
 
-  func and(_ s : Term, _ t: Term) -> Term {
-    return yices_and2(s, t)
-  }
-
-  func and(_ ts: [Term]) -> Term {
-    var ts_copy = ts
-    return yices_and(UInt32(ts.count), &ts_copy)
-  }
-
-  func or(_ s: Term, _ t: Term) -> Term {
-    return yices_or2(s, t)
-  }
-
-  func or(_ ts: [Term]) -> Term {
-    var ts_copy = ts
-    return yices_or(UInt32(ts.count), &ts_copy)
-  }
-
-  func implies(_ s: Term, _ t: Term) -> Term {
-    return yices_implies(s, t)
-  }
-
-  func ite(_ c: Term, t: Term, f: Term) -> Term {
-      return yices_ite(c, t, f)
-  }
-
-  // arithmetic operators
-  func eq(_ s: Term, _ t: Term) -> Term {
-    return yices_eq(s, t)
-  }
-
-  func neq(_ s : Term, _ t: Term) -> Term {
-    return yices_neq(s, t)
-  }
-
-  func gt(_ s: Term, _ t: Term) -> Term {
-    return yices_arith_gt_atom(s, t)
-  }
-
-  func ge(_ s: Term, _ t: Term) -> Term {
-    return yices_arith_geq_atom(s, t)
-  }
-
-  func add(_ s: Term, _ t: Term) -> Term {
-    return yices_add(s, t)
+  func mkAnd(_ ts: [Expr]) -> Expr {
+    var ts_copy = ts.map{ $0.expr }
+    return YicesExpr(yices_and(UInt32(ts.count), &ts_copy))
   }
   
-  // create terms
-  func freshVar(_ name: String, _ type: term_t) -> Term  {
+  func freshVar(_ name: String, _ type: term_t) -> Expr  {
     let t = yices_new_uninterpreted_term(type)
     let code = yices_set_term_name(t, name)
     // FIXME: rather throw exception
-    guard (code >= 0) else { yices_print_error(stdout); return bot }
-    return t
+    guard (code >= 0) else { yices_print_error(stdout); return mkBot }
+    return YicesExpr(t)
   }
 
-  func freshBoolVar(_ name: String) -> Term {
+  func mkBoolVar(_ name: String) -> Expr {
     return freshVar(name, bool_type)
   }
 
-  func freshIntVar(_ name: String) -> Term {
+  func mkIntVar(_ name: String) -> Expr {
     return freshVar(name, int_type)
   }
 
@@ -108,7 +132,7 @@ class YicesLogic : Logic {
     return tau
   }
 
-  private static func typedSymbol(_ symbol: String, _ type: TType) -> Term {
+  private static func typedSymbol(_ symbol: String, _ type: TType) -> Expr {
     assert(!symbol.isEmpty, "a symbol name must not be empty")
 
     var c = yices_get_term_by_name(symbol)
@@ -120,21 +144,21 @@ class YicesLogic : Logic {
       assert (type == ctype,
         "\(String(tau:type)) != \(String(tau: ctype)) \(String(term:c)) for '\(symbol)'")
     }
-    return c
+    return Expr(c)
   }
 
   /// Get or create a global constant `symbol` of type `term_tau`
-  func getConst(_ symbol: String, _ type: TType) -> Term {
+  func constant(_ symbol: String, _ type: TType) -> Expr {
     return YicesLogic.typedSymbol(symbol, type)
   }
 
   /// Create a homogenic domain tuple
-  func getDomain(_ count: Int, _ type: TType) -> [TType] {
+  func domain(_ count: Int, _ type: TType) -> [TType] {
     return [TType](repeating: type, count: count)
   }
 
   /// Get or create a function symbol of type domain -> range
-  func getFun(_ symbol: String, _ domain: [TType], _ range: TType) -> Term {
+  func function(_ symbol: String, _ domain: [TType], _ range: TType) -> Expr {
     let type_f = yices_function_type(UInt32(domain.count), domain, range)
     return YicesLogic.typedSymbol(symbol, type_f)
   }
@@ -142,32 +166,45 @@ class YicesLogic : Logic {
   /// Create application of uninterpreted function or predicate named `symbol`
   /// with arguments `args` of implicit global type `free_type`
   /// and explicit return type `type`
-  func getApp(_ symbol: String, _ args: [Term], _ range: TType) -> Term {
-    guard args.count > 0 else { return getConst(symbol, range) }
+  func app(_ symbol: String, _ args: [Expr], _ range: TType) -> Expr {
+    guard args.count > 0 else { return constant(symbol, range) }
 
-    let f = getFun(symbol, getDomain(args.count, free_type), range)
-    return yices_application(f, UInt32(args.count), args)
+    let f = function(symbol, domain(args.count, free_type), range)
+    let nargs = UInt32(args.count)
+    return YicesExpr(yices_application(f.expr, nargs, args.map{ $0.expr }))
   }
 
-	// term deconstruction
-  /// Get children of a yices term.
-  func children(_ term: Term) -> [Term] {
-    return (0..<yices_term_num_children(term)).map { yices_term_child(term, $0) }
-  }
 
-  func subterms(_ term: Term) -> Set<Term> {
-    var terms = Set(children(term).flatMap { subterms($0) })
-    terms.insert(term)
-    return terms
-  }
+  // assertion and checking
+  func ensure(_ formula: Expr) {
+		yices_assert_formula(ctx, formula.expr)
+	}
+
+  func ensureCheck(formula: Expr) -> Bool {
+		ensure(formula)
+		return isSatisfiable
+	}
+
+	var isSatisfiable: Bool {
+		switch yices_check_context(ctx, nil) {
+			case STATUS_SAT:
+				return true
+			case STATUS_UNSAT:
+				return false
+			default:
+				print("-------------------------------------------------------")
+				assert(false)
+				return true
+		}
+	}
 
   // evaluation
   /// Evaluate a boolean term `t` in `model`
-  func evalBool(_ model: Model, _ t: Term) -> Bool {
-    assert (bool_type == yices_type_of_term(t))
+  func evalBool(_ model: Model, _ t: Expr) -> Bool {
+    assert (bool_type == yices_type_of_term(t.expr))
 
 		var p : Int32 = 0
-    let success = yices_get_bool_value(model, t, &p)
+    let success = yices_get_bool_value(model, t.expr, &p)
     // FIXME: rather throw exception
     guard success == 0  else {
       yices_print_error(stdout)
@@ -177,11 +214,11 @@ class YicesLogic : Logic {
   }
 
   /// Evaluate a term `t` of integer type in `model`
-  func evalInt(_ model: Model, _ t: Term) -> Int {
-    assert (int_type == yices_type_of_term(t))
+  func evalInt(_ model: Model, _ t: Expr) -> Int {
+    assert (int_type == yices_type_of_term(t.expr))
 
 		var p : Int32 = -1
-    let success = yices_get_int32_value(model, t, &p)
+    let success = yices_get_int32_value(model, t.expr, &p)
     // FIXME: rather throw exception
     guard success == 0  else {
       yices_print_error(stdout)
