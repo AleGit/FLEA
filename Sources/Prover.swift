f4596b809498ed622322ce36d3b76b5bb34f3937
Alexander Maringele <aXm@ydWLT-E-17-120.uibk.ac.at>
2016-10-11 13:28:20 +0200

    Update Prover comments

--- Sources/Prover.swift
+++ Sources/Prover.swift
@@ -13,7 +13,10 @@ protocol Prover {
     /// returns false if problem is not a theorem, e.g. set of clause is saturated and satisfiable
     func run(timeout: TimeInterval) -> Bool?
 
+    /// report the number of files read from storage
     var fileCount: Int { get }
+
+    /// report the number of clauses in prover repository
     var clauseCount: Int { get }
 }
 
@@ -30,28 +33,40 @@ protocol YicesProver: Prover {
 // MARK: - protocol extensions
 
 extension Prover {
+    /// Searches for a problem by name, convention, and TPTP Path,
+    /// e.g. "PUZ001-1" => ~/TPTP/Problems/PUZ001-1p
+    /// It will return
+    /// - a pair with the problem file url and content if successful
+    /// - nil if problem file could not be located, read or parsed.
     static func URLAndFile(problem name: String) -> (URL, Tptp.File)? {
+        /// find an accessible problem file(with $name extension '.p')
         guard let url = URL(fileURLWithProblem: name) else {
-            Syslog.error { "Problem \(name) could not be found." }
+            Syslog.error {  "Problem \(name) could not be found." }
             return nil
         }
+        /// parse accessible problem file
         guard let file = Tptp.File(url:url) else {
-            Syslog.error { "Problem \(name) at \(url.path) could not be read and parsed." }
+            Syslog.error { "Problem \(name) at \(url.path) could not be read or parsed." }
             return nil
         }
         return (url, file)
     }
 
+    /// Search and parse an axiom file by name, problem url, conventions, and TPTP Path,
+    /// e.g. the search starts relatively to the problem file.
+    /// It will return
+    /// - a pair with the axiom file url and content if successful
+    /// - nil if axiom file could not be located, read or parsed.
     static func URLAndFile(axiom name: String, problemURL: URL?) -> (URL, Tptp.File)? {
+        /// find an accessible axiom file (with $name and extension '.ax')
         guard let url = URL(fileURLWithAxiom: name, problemURL: problemURL) else {
             Syslog.error { "Axiom \(name) could not be found. (Problem: \(problemURL?.path))" }
             return nil
         }
 
+        /// parse accessible axiom file
         guard let file = Tptp.File(url:url) else {
-            Syslog.error {
-                "Axiom \(name) at \(url.path) could not be read or parsed."
-                }
+            Syslog.error { "Axiom \(name) at \(url.path) could not be read or parsed." }
             return nil
         }
         return (url, file)
