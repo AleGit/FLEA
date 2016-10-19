cb520ccc9a91f4a6ac15d70a3d652b2a0cbbf6a8
Alexander Maringele <aXm@ydWLT-E-17-120.uibk.ac.at>
2016-10-19 09:09:47 +0200

    Update Proverlet with procedure outline

--- Sources/Proverlet.swift
+++ Sources/Proverlet.swift
@@ -1,3 +1,7 @@
+
+
+
+
 final class Proverlet<N:Node>: Prover
 where N:SymbolStringTyped {
     /// List of (file path, number of clauses) pairs.
@@ -5,6 +9,14 @@ where N:SymbolStringTyped {
 
     /// List of (clause name, clause role, clause) triples.
     fileprivate var parsedClauses: Array<(String, Tptp.Role, N)>
+
+    /// index structure to find clashing selected literals
+    fileprivate var literals = TrieClass<SymHop<N.Symbol>, Int>()
+
+    /// index structure to find clause variants
+    fileprivate var variants = TrieClass<N.Symbol, Int>()
+
+
 
     init?(problem name: String) {
         Syslog.info { "problem name = \(name)" }
@@ -48,7 +60,29 @@ where N:SymbolStringTyped {
 
 extension Proverlet {
 
-
+    /*
+        adoption of the given clause algorithm (reference?)
+        ==================================
+        - initialize a list of passive clauses with the clauses of a problem
+        - create an empty list of usable clauses
+        - this may loop forever (e.g. satisfiable with infinite model)
+          or exceed all reasonable costs (time and space)
+            1. select a clause c_i from passive clauses (by a strategy)
+            2. assert grounded c_i in SMT solver context
+            3. goto 12 if not SAT(context)
+            4. get a model from satisfiable context
+            5. check selected literals of usable clauses and
+               unselect selected literals that not hold in actual model
+            6. add clause c_i to usable clauses, remove c_i from passive clauses
+            7. select a clause c_j without a selected literal from usable clauses, e.g c_i
+            8. select literal from c_j and search for clashing selected literals
+            9. derive clauses by clashes and equality axioms and add
+               new clauses (i.e. no variants exsit) to the passive clauses.
+            10. goto 7 if at least one usable clause without a selected literal is left.
+            11. goto 1 if at least one passive clause is left.
+            12. SAT(problem) <=> SAT(context)
+        - maintain index structures for the search for variants and clashing literals.
+        */
     func run(timeout: TimeInterval) -> Bool? {
         Syslog.fail { "MISSING IMPLEMENTATION" }
         return nil
