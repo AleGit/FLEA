9f035b3d7a82be59c002f98e55073c842b7c1dcb
Alexander Maringele <aXm@ydWLT-E-17-120.uibk.ac.at>
2016-10-19 15:19:52 +0200

    Update node properties tests

--- Tests/FLEATests/NodePropertiesTests.swift
+++ Tests/FLEATests/NodePropertiesTests.swift
@@ -23,14 +23,16 @@ public class NodePropertiesTests: FleaTestCase {
   }
 
   // local private adoption of protocol to avoid any side affects
-  final class N: SymbolStringTyped, SymbolTabulating,
+  private final class FullNode: SymbolStringTyped, SymbolTabulating,
   Sharing, Kin, Node, ExpressibleByStringLiteral {
     static var symbols = StringIntegerTable<Int>()  // : SymbolTabulating
-    static var pool = WeakSet<N>()                  // : Sharing
-    var folks =  WeakSet<N>()                       // : Kin
+    static var pool = WeakSet<FullNode>()                  // : Sharing
+    var folks =  WeakSet<FullNode>()                       // : Kin
 
-    var symbol: Int = N.symbolize(string:"*", type:.variable)       // : Node
-    var nodes: [N]? = nil                                           // : Node
+    var symbol: Int = FullNode.symbolize(string:"*", type:.variable)       // : Node
+    var nodes: [FullNode]? = nil                                           // : Node
+
+    // lazy evaluation and memorizing of node properties
 
     lazy var hashValue: Int = self.defaultHashValue
     lazy var description: String = self.defaultDescription
@@ -39,94 +41,97 @@ public class NodePropertiesTests: FleaTestCase {
     lazy var width: Int = self.defaultWidth
     lazy var size: Int = self.defaultSize
 
+    // test terms
 
-    static let a = "a" as N
-    static let X = "X" as N
-    static let faX = "f(a,X)" as N
-    static let f2 = "f(g(X),f(a,g(b)))" as N
 
-    static let _a = "a" as N
-    static let _X = "X" as N
-    static let _faX = "f(a,X)" as N
-    static let _f2 = "f(g(X),f(a,g(b)))" as N
+    static let a = "a" as FullNode
+    static let X = "X" as FullNode
+    static let faX = "f(a,X)" as FullNode
+    static let f2 = "f(g(X),f(a,g(b)))" as FullNode
+    static let clause = "p(X)|q(a)" as FullNode
+
+    static let _a = "a" as FullNode
+    static let _X = "X" as FullNode
+    static let _faX = "f(a,X)" as FullNode
+    static let _f2 = "f(g(X),f(a,g(b)))" as FullNode
   }
 
   func testHeight() {
-    XCTAssertEqual(0, N.a.height, nok)
-    XCTAssertEqual(0, N.X.height, nok)
-    XCTAssertEqual(1, N.faX.height, nok)
+    XCTAssertEqual(0, FullNode.a.height, nok)
+    XCTAssertEqual(0, FullNode.X.height, nok)
+    XCTAssertEqual(1, FullNode.faX.height, nok)
   }
 
   func testWidth() {
-    XCTAssertEqual(1, N.a.width, nok)
-    XCTAssertEqual(1, N.X.width, nok)
-    XCTAssertEqual(2, N.faX.width, nok)
+    XCTAssertEqual(1, FullNode.a.width, nok)
+    XCTAssertEqual(1, FullNode.X.width, nok)
+    XCTAssertEqual(2, FullNode.faX.width, nok)
   }
 
   func testSize() {
-    XCTAssertEqual(1, N.a.size, nok)
-    XCTAssertEqual(1, N.X.size, nok)
-    XCTAssertEqual(3, N.faX.size, nok)
+    XCTAssertEqual(1, FullNode.a.size, nok)
+    XCTAssertEqual(1, FullNode.X.size, nok)
+    XCTAssertEqual(3, FullNode.faX.size, nok)
   }
 
   func testSharing() {
-      XCTAssertTrue(N.a === N._a, nok)
-      XCTAssertTrue(N.X === N._X, nok)
-      XCTAssertTrue(N.faX === N._faX, nok)
+      XCTAssertTrue(FullNode.a === FullNode._a, nok)
+      XCTAssertTrue(FullNode.X === FullNode._X, nok)
+      XCTAssertTrue(FullNode.faX === FullNode._faX, nok)
 
-      XCTAssertTrue(N.a === N.faX.nodes!.first!, nok)
-      XCTAssertTrue(N.X === N.faX.nodes!.last!, nok)
+      XCTAssertTrue(FullNode.a === FullNode.faX.nodes!.first!, nok)
+      XCTAssertTrue(FullNode.X === FullNode.faX.nodes!.last!, nok)
 
-      XCTAssertTrue(N.a === N.f2.nodes!.last!.nodes!.first!, nok)
-      XCTAssertTrue(N.X === N.f2.nodes!.first!.nodes!.first!, nok)
+      XCTAssertTrue(FullNode.a === FullNode.f2.nodes!.last!.nodes!.first!, nok)
+      XCTAssertTrue(FullNode.X === FullNode.f2.nodes!.first!.nodes!.first!, nok)
 
-      XCTAssertTrue(N.f2 === N._f2, nok)
+      XCTAssertTrue(FullNode.f2 === FullNode._f2, nok)
   }
 
   func testKin() {
-      let _ = [N.a, N.X, N.faX, N.f2]
+      let _ = [FullNode.a, FullNode.X, FullNode.faX, FullNode.f2, FullNode.clause]
 
-      var expected: [N] = [ N.faX, "f(a,g(b))" ]
-      XCTAssertEqual(Set(expected), Set(N.a.folks), nok)
+      var expected: [FullNode] = [ FullNode.faX, "f(a,g(b))", FullNode.clause.nodes!.last! ]
+      XCTAssertEqual(Set(expected), Set(FullNode.a.folks), nok)
 
-      expected = [ N.faX, "g(X)" ]
-      XCTAssertEqual(Set(expected), Set(N.X.folks), nok)
+      expected = [ FullNode.faX, "g(X)", FullNode.clause.nodes!.first! ]
+      XCTAssertEqual(Set(expected), Set(FullNode.X.folks), nok)
 
-      expected = [N]()
-      XCTAssertEqual(Set(expected), Set(N.faX.folks), nok)
+      expected = [FullNode]()
+      XCTAssertEqual(Set(expected), Set(FullNode.faX.folks), nok)
 
-      expected = [N]()
-      XCTAssertEqual(Set(expected), Set(N.f2.folks), nok)
+      expected = [FullNode]()
+      XCTAssertEqual(Set(expected), Set(FullNode.f2.folks), nok)
 
-      expected = [ N.f2 ]
-      XCTAssertEqual(Set(expected), Set(("g(X)" as N).folks), nok)
-      XCTAssertEqual(Set(expected), Set(("f(a,g(b))" as N).folks), nok)
+      expected = [ FullNode.f2 ]
+      XCTAssertEqual(Set(expected), Set(("g(X)" as FullNode).folks), nok)
+      XCTAssertEqual(Set(expected), Set(("f(a,g(b))" as FullNode).folks), nok)
 
-      expected = [ N.faX, "f(a,g(b))", "g(a)" ]
-      XCTAssertEqual(Set(expected), Set(N.a.folks), nok)
+      expected = [ FullNode.faX, "f(a,g(b))", "g(a)", FullNode.clause.nodes!.last!]
+      XCTAssertEqual(Set(expected), Set(FullNode.a.folks), nok)
 
-      expected = [ "g(f(a,X))"]
-      XCTAssertEqual(Set(expected), Set(N.faX.folks), nok)
+      expected = [ "g(f(a,X))" ]
+      XCTAssertEqual(Set(expected), Set(FullNode.faX.folks), nok)
   }
 
   func testHashValue() {
 
-      XCTAssertEqual(N.faX.hashValue, N.faX.defaultHashValue, nok)
+      XCTAssertEqual(FullNode.faX.hashValue, FullNode.faX.defaultHashValue, nok)
 
-      XCTAssertNotEqual(N.a.hashValue, N.X.hashValue, nok)
-      XCTAssertNotEqual(N.a.hashValue, N.faX.hashValue, nok)
-      XCTAssertNotEqual(N.a.hashValue, N.f2.hashValue, nok)
+      XCTAssertNotEqual(FullNode.a.hashValue, FullNode.X.hashValue, nok)
+      XCTAssertNotEqual(FullNode.a.hashValue, FullNode.faX.hashValue, nok)
+      XCTAssertNotEqual(FullNode.a.hashValue, FullNode.f2.hashValue, nok)
 
-      XCTAssertNotEqual(N.X.hashValue, N.faX.hashValue, nok)
-      XCTAssertNotEqual(N.X.hashValue, N.f2.hashValue, nok)
+      XCTAssertNotEqual(FullNode.X.hashValue, FullNode.faX.hashValue, nok)
+      XCTAssertNotEqual(FullNode.X.hashValue, FullNode.f2.hashValue, nok)
 
-      XCTAssertNotEqual(N.faX.hashValue, N.f2.hashValue, nok)
+      XCTAssertNotEqual(FullNode.faX.hashValue, FullNode.f2.hashValue, nok)
   }
 
   func testDescription() {
-      XCTAssertEqual("f(a,X)", N.faX.defaultDescription, nok)
-      XCTAssertEqual("f(g(X),f(a,g(b)))", N.f2.defaultDescription, nok)
-      XCTAssertEqual(N.faX.description, N.faX.defaultDescription, nok)
+      XCTAssertEqual("f(a,X)", FullNode.faX.defaultDescription, nok)
+      XCTAssertEqual("f(g(X),f(a,g(b)))", FullNode.f2.defaultDescription, nok)
+      XCTAssertEqual(FullNode.faX.description, FullNode.faX.defaultDescription, nok)
 
   }
 }
