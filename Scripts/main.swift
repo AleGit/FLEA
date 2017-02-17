8a4e47a702c2a20ab154638dd2f99ec0f44e9701
Alexander Maringele <nyaya@maringele.at>
2017-02-17 11:00:01 +0100

    Swiftformat

--- Scripts/main.swift
+++ Scripts/main.swift
@@ -1,52 +1,50 @@
 import Foundation
 
-Syslog.openLog(options:.console, .pid, .perror)
+Syslog.openLog(options: .console, .pid, .perror)
 let logLevel = Syslog.maximalLogLevel
-let _ = Syslog.setLogMask(upTo:logLevel)
+_ = Syslog.setLogMask(upTo: logLevel)
 defer {
     Syslog.closeLog()
 }
 
-
-
 private final class TheNode: SymbolStringTyped, SymbolTabulating, Sharing, Node,
-  ExpressibleByStringLiteral {
+    ExpressibleByStringLiteral {
     typealias S = Int
     typealias N = TheNode
-    static var symbols = StringIntegerTable<S>()    // protocol SymbolTabulating
-    static var pool = WeakSet<N>()                  // protocol Sharing
+    static var symbols = StringIntegerTable<S>() // protocol SymbolTabulating
+    static var pool = WeakSet<N>() // protocol Sharing
     // var folks = WeakSet<N>()                  // protocol Kin
 
-    var symbol: S = N.symbolize(string:Tptp.wildcard, type:.variable)
-    var nodes: [N]? = nil                           // protocol Node
+    var symbol: S = N.symbolize(string: Tptp.wildcard, type: .variable)
+    var nodes: [N]? // protocol Node
 
-    var description: String {                       // protocol Node : CustomStringConvertible
+    var description: String { // protocol Node : CustomStringConvertible
         return defaultDescription
     }
 
     lazy var hashValue: Int = self.defaultHashValue // protocol Node : Hashable
-  }
+}
 
 // MARK: functions
 func process(problem: String) {
 
     Syslog.debug { "Hello, \(problem)" }
 
-    guard let theProver = FLEA.ProverY<TheNode>(problem:problem) else {
+    guard let theProver = FLEA.ProverY<TheNode>(problem: problem) else {
         Syslog.warning { "Could not create prover with problem \(problem)" }
-          return
-      }
-      print(problem, theProver.clauseCount, theProver.insuredClausesCount)
+        return
+    }
+    print(problem, theProver.clauseCount, theProver.insuredClausesCount)
 
     let (result, runtime) = utileMeasure {
-        theProver.run(timeout:60.0)
+        theProver.run(timeout: 60.0)
     }
 
     print(result, runtime, "clauses:", theProver.clauseCount,
-    "ensured:", theProver.insuredClausesCount)
-
+          "ensured:", theProver.insuredClausesCount)
 }
-let _ = Demo.demo()
+
+_ = Demo.demo()
 
 // ============================================================/
 
@@ -63,8 +61,6 @@ if let problems = options["--problem"] {
 
     for problem in problems {
         print("Problem:\(problem)")
-        process(problem:problem)
+        process(problem: problem)
     }
 }
-
-
